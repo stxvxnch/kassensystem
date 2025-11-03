fe01fcaefbdd5f5bbcba54dcf15c7bb946419188
Steven Kane <steven@kane-online.de>
2025-11-03 15:54:57 +0100

    Finished Layout and startet JavaScript with some excersises

---
+++ script.js
@@ -0,0 +1,6 @@
+const plus1 = document.getElementById('plus1');
+
+plus1.addEventListener('click', function(){
+    const produktName = document.getElementById('produkt1').textContent
+    console.log(produktName)
+})
