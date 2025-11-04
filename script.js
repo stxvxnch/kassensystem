74ee1bc296c86b9f5607ac4eee3020fe9592ba55
Steven Kane <steven@kane-online.de>
2025-11-04 15:48:48 +0100

    Add new logic to js, so current Order is shown

--- script.js
+++ script.js
@@ -1,6 +1,62 @@
-const plus1 = document.getElementById('plus1');
+// script.js
 
-plus1.addEventListener('click', function(){
-    const produktName = document.getElementById('produkt1').textContent
-    console.log(produktName)
-})
+// Alle Produkte holen
+const products = document.querySelectorAll('.product');
+const orderList = document.querySelector('.order-list');
+
+products.forEach(product => {
+    const plusBtn = product.querySelector('.btn-plus');
+    const productName = product.querySelector('.produkt-name').textContent;
+    const minusBtn = product.querySelector(".btn-minus");
+    const productPrice = product.querySelector('.produkt-preis').textContent;
+    const priceNumber = parseFloat(productPrice.replace('€', '').trim());
+
+
+    // Klick auf + Button
+    plusBtn.addEventListener('click', () => {
+        addToOrder(productName, priceNumber);
+    });
+
+    minusBtn.addEventListener("click", () => {
+        deleteFromOrder(productName, priceNumber)
+    })
+});
+
+// Funktion um Produkt in die Bestellung zu bringen
+function addToOrder(name, price) {
+    // Prüfen, ob das Produkt schon in der Bestellung ist
+    let existingItem = Array.from(orderList.children).find(item => item.dataset.name === name);
+
+    if (existingItem) {
+        // Anzahl erhöhen
+        let countElem = existingItem.querySelector('.count');
+        countElem.textContent = parseInt(countElem.textContent) + 1;
+    } else {
+        // Neues Produkt erstellen
+        const orderItem = document.createElement('div');
+        orderItem.className = 'order-item';
+        orderItem.dataset.name = name;
+        orderItem.dataset.name = price;
+        orderItem.innerHTML = `${name} - Menge: <span class="count">1</span>`;
+        orderItem.innerHTML = `${price} €`
+        orderList.appendChild(orderItem);
+        console.log(price)
+    }
+}
+
+function deleteFromOrder(name, price) {
+    let existingItem = Array.from(orderList.children).find(item => item.dataset.name === name)
+
+    if(existingItem){
+        let countElem = existingItem.querySelector(".count")
+        let currentCount = parseInt(countElem.textContent)
+
+        if(currentCount > 1){
+            countElem.textContent = parseInt(countElem.textContent) - 1;
+        } else {
+            orderList.removeChild(existingItem)
+        }
+        
+    }
+}
+
